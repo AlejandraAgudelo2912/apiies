390a6f21ed9348bd6a28ba4062b5c9b4bdd44e84
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2024-10-24 16:49:13 +0200

    Fin paso 1

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name'=>fake()->words(asText: true)//true para las palbaras que me devuelva sea solo texto
         ];
     }
 }
